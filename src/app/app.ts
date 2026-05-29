ba28058dfcbdc35624600c60d4e1419bd828a5b0
Rodrigo Sandri <seuemail@example.com>
2026-05-29 17:54:48 -0300

    feat: adiciona componentes de gerenciamento de chuteiras no Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Navbar} from './components/navbar/navbar';
+import { ChuteiraForm } from './components/chuteira-form/chuteira-form';
+import { ChuteiraList } from './components/chuteira-list/chuteira-list';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar],
+  imports: [Navbar, ChuteiraForm, ChuteiraList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
