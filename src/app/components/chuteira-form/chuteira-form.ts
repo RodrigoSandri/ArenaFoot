0b412a00f98cfeed80d7ef5267d98a17e5a91dc3
Rodrigo Sandri <seuemail@example.com>
2026-05-31 17:35:00 -0300

    feat: adicionar criação de chuteiras com emissão de evento após salvar

--- src/app/components/chuteira-form/chuteira-form.ts
+++ src/app/components/chuteira-form/chuteira-form.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, EventEmitter, inject, output, Output, ɵinjectChangeDetectorRef } from '@angular/core';
 import { ChuteiraService } from '../../services/chuteira.service';
 import { Chuteira } from '../../models/chuteira';
 
@@ -18,16 +18,20 @@ export class ChuteiraForm {
     preco: 0,
   };
 
+  @Output() salvo = new EventEmitter<void>();
+
+  private readonly cdr = inject(ɵinjectChangeDetectorRef);
   private readonly chuteiraService = inject(ChuteiraService);
+
 
   salvar(): void {
     this.chuteiraService.criar(this.chuteira).subscribe({
       next: () => {
-        alert('Chuteira salva com sucesso!');
-      },
-      error: (err) => {
-        console.error('Erro ao salvar chuteira:', err);
-      }
-    });
+        this.chuteira = { nome: '', 
+          descricao: '', preco: 0}
+          this.salvo.emit();
+          this.cdr.detectChanges();
+      }    
+    })
   }
 }
